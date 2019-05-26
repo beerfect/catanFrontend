8fbeeba593b6bef3cb96ffc2b66dd051054730a5
beerfect <beerfect@yandex.ru>
2019-05-26 16:33:25 +0300

    bugfix

--- js/interface.js
+++ js/interface.js
@@ -10,4 +10,61 @@ clearHexFormDice(hex00);                      // очистить хекс от 
 clearHexFormTile(hex00);                      // очистить хекс от тайла
 clearNode(node00);                            //очистить узел от поселения/города
 
-// ИНТЕРФЕЙС (END)
+ИНТЕРФЕЙС (END)
+
+// TESTS (START) 
+
+// createTileOnHex(forest,hex00); 
+// createTileOnHex(desert,hex01); 
+// createTileOnHex(pasture,hex02); 
+// createTileOnHex(mountains,hex03); 
+// createTileOnHex(hills,hex04); 
+// createTileOnHex(forest,hex05); 
+// createTileOnHex(mountains,hex06); 
+// createTileOnHex(hills,hex07); 
+// createTileOnHex(forest,hex08); 
+// createTileOnHex(desert,hex09); 
+// createTileOnHex(pasture,hex10); 
+// createTileOnHex(desert,hex11); 
+
+// setDiceOnHex(diceA,hex00); 
+// setDiceOnHex(diceB,hex01); 
+// setDiceOnHex(diceC,hex02); 
+// setDiceOnHex(diceD,hex03); 
+// setDiceOnHex(diceE,hex04); 
+// setDiceOnHex(diceF,hex05); 
+// setDiceOnHex(diceG,hex06); 
+// setDiceOnHex(diceH,hex07); 
+// setDiceOnHex(diceI,hex08); 
+// setDiceOnHex(diceJ,hex09); 
+// setDiceOnHex(diceK,hex10); 
+
+// addPlayersSettlementOnNone(player00,node00); 
+// addPlayersSettlementOnNone(player00,node01); 
+// addPlayersSettlementOnNone(player00,node02); 
+
+// addPlayersSettlementOnNone(player01,node03); 
+// addPlayersSettlementOnNone(player01,node04); 
+// addPlayersSettlementOnNone(player01,node05); 
+// addPlayersSettlementOnNone(player01,node06); 
+
+// addPlayersCityToNode(player02,node07);
+// addPlayersCityToNode(player02,node08);
+// addPlayersCityToNode(player02,node09);
+// addPlayersCityToNode(player02,node10);
+
+// clearHexFormTile(hex00); 
+// clearHexFormTile(hex01); 
+// clearHexFormTile(hex02); 
+
+// clearHexFormDice(hex07);  
+// clearHexFormDice(hex08);  
+// clearHexFormDice(hex09);  
+// clearHexFormDice(hex10);
+
+// clearNode(node03);
+// clearNode(node04);
+// clearNode(node05);
+// clearNode(node06);
+
+// TESTS (END)
